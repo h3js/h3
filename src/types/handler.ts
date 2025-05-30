f69de6818096faf87c0c4b8191c1901d17a93002
Pooya Parsa <pooya@pi0.io>
2025-05-30 23:18:41 +0200

    cleanup

--- src/types/handler.ts
+++ src/types/handler.ts
@@ -1,5 +1,14 @@
+import type { MaybePromise } from "./_utils.ts";
 import type { H3Event } from "./event.ts";
-import type { H3 } from "../h3.ts";
+
+//  --- event handler ---
+
+export interface EventHandler<
+  Request extends EventHandlerRequest = EventHandlerRequest,
+  Response extends EventHandlerResponse = EventHandlerResponse,
+> {
+  (event: H3Event<Request>): Response;
+}
 
 export interface EventHandlerRequest {
   body?: unknown;
@@ -8,21 +17,6 @@ export interface EventHandlerRequest {
 }
 
 export type EventHandlerResponse<T = unknown> = T | Promise<T>;
-
-export type InferEventInput<
-  Key extends keyof EventHandlerRequest,
-  Event extends H3Event,
-  T,
-> = void extends T ? (Event extends H3Event<infer E> ? E[Key] : never) : T;
-
-type MaybePromise<T> = T | Promise<T>;
-
-export interface EventHandler<
-  Request extends EventHandlerRequest = EventHandlerRequest,
-  Response extends EventHandlerResponse = EventHandlerResponse,
-> extends Partial<Pick<H3, "handler" | "config">> {
-  (event: H3Event<Request>): Response;
-}
 
 //  --- middleware ---
 
@@ -47,3 +41,11 @@ export type LazyEventHandler = () => EventHandler | Promise<EventHandler>;
 export interface DynamicEventHandler extends EventHandler {
   set: (handler: EventHandler) => void;
 }
+
+// --- utils ---
+
+export type InferEventInput<
+  Key extends keyof EventHandlerRequest,
+  Event extends H3Event,
+  T,
+> = void extends T ? (Event extends H3Event<infer E> ? E[Key] : never) : T;
