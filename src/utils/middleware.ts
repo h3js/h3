61a65acd88bdeb76e16b040a7ef9fdf3862bc351
Pooya Parsa <pooya@pi0.io>
2025-06-09 23:21:16 +0200

    refactor: consistent hooks

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,4 +1,7 @@
-import type { HTTPError } from "../error.ts";
+import { HTTPError } from "../error.ts";
+import { handleResponse } from "../response.ts";
+import type { MaybePromise } from "../types/_utils.ts";
+
 import type { H3Event } from "../types/event.ts";
 import type { Middleware } from "../types/handler.ts";
 
@@ -15,28 +18,50 @@ export function onRequest(
 
 /**
  * Define a middleware that runs after Response is generated.
+ *
+ * You can return a new Response from the handler to replace the original response.
  */
 export function onResponse(
-  handler: (event: H3Event, res: { body: any }) => void | Promise<void>,
+  handler: (
+    response: Response,
+    event: H3Event,
+  ) => MaybePromise<void | Response>,
 ): Middleware {
   return async (event, next) => {
-    const body = await next();
-    await handler(event, { body });
-    return body;
+    const rawBody = await next();
+    const response = await handleResponse(rawBody, event);
+    const newResponse = await handler(response, event);
+    return newResponse || response;
   };
 }
 
 /**
  * Define a middleware that runs when an error occurs.
+ *
+ * You can return a new Response from the handler to gracefully handle the error.
  */
 export function onError(
-  handler: (event: H3Event, error: Error | HTTPError) => void | Promise<void>,
+  handler: (error: HTTPError, event: H3Event) => MaybePromise<void | unknown>,
 ): Middleware {
   return async (event, next) => {
     try {
       return await next();
-    } catch (error) {
-      await handler(event, error as Error | HTTPError);
+    } catch (rawError: any) {
+      const isHTTPError = HTTPError.isError(rawError);
+      const error = isHTTPError
+        ? (rawError as HTTPError)
+        : new HTTPError(rawError);
+      if (!isHTTPError) {
+        // @ts-expect-error unhandled is readonly for public interface
+        error.unhandled = true;
+        if (rawError?.stack) {
+          error.stack = rawError.stack;
+        }
+      }
+      const newResponse = await handler(error, event);
+      if (newResponse !== undefined) {
+        return newResponse;
+      }
       throw error;
     }
   };
