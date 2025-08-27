0727bae0f47442cfe5ec6b9277c084a74c006d0b
Pooya Parsa <pooya@pi0.io>
2025-08-27 16:13:37 +0200

    merge tests

--- test/cookies.test.ts
+++ test/cookies.test.ts
@@ -1,4 +1,10 @@
-import { getCookie, parseCookies, setCookie } from "../src/utils/cookie.ts";
+import {
+  getCookie,
+  parseCookies,
+  setCookie,
+  getChunkedCookie,
+  setChunkedCookie,
+} from "../src/utils/cookie.ts";
 import { describeMatrix } from "./_setup.ts";
 
 describeMatrix("cookies", (t, { it, expect, describe }) => {
@@ -105,5 +111,96 @@ describeMatrix("cookies", (t, { it, expect, describe }) => {
       "session=987; Path=/; SameSite=Strict",
     ]);
     expect(await result.text()).toBe("200");
+  });
+
+  describeMatrix("chunked", (t, { it, expect, describe }) => {
+    describe("getChunkedCookie", () => {
+      it("can parse cookie that is chunked", async () => {
+        t.app.get("/", (event) => {
+          const authorization = getChunkedCookie(event, "Authorization");
+          expect(authorization).toEqual("123456789");
+          return "200";
+        });
+
+        const result = await t.fetch("/", {
+          headers: {
+            Cookie: [
+              "Authorization=chunks.3",
+              "Authorization.C1=123",
+              "Authorization.C2=456",
+              "Authorization.C3=789",
+            ].join("; "),
+          },
+        });
+
+        expect(await result.text()).toBe("200");
+      });
+
+      it("can parse cookie that is not chunked", async () => {
+        t.app.get("/", (event) => {
+          const authorization = getChunkedCookie(event, "Authorization");
+          expect(authorization).toEqual("not-chunked");
+          return "200";
+        });
+
+        const result = await t.fetch("/", {
+          headers: {
+            Cookie: ["Authorization=not-chunked"].join("; "),
+          },
+        });
+
+        expect(await result.text()).toBe("200");
+      });
+    });
+
+    describe("setChunkedCookie", () => {
+      it("can set-cookie with setChunkedCookie", async () => {
+        t.app.get("/", (event) => {
+          setChunkedCookie(
+            event,
+            "Authorization",
+            "1234567890ABCDEFGHIJXYZ",
+            {},
+            10,
+          );
+          return "200";
+        });
+        const result = await t.fetch("/");
+        expect(result.headers.getSetCookie()).toEqual([
+          "Authorization=chunks.3; Path=/",
+          "Authorization.C1=1234567890; Path=/",
+          "Authorization.C2=ABCDEFGHIJ; Path=/",
+          "Authorization.C3=XYZ; Path=/",
+        ]);
+        expect(await result.text()).toBe("200");
+      });
+
+      it("smaller set-cookie removes superfluous chunks", async () => {
+        // set smaller cookie with fewer chunks, should have deleted superfluous chunks
+        t.app.get("/", (event) => {
+          setChunkedCookie(event, "Authorization", "0000100002", {}, 5);
+          return "200";
+        });
+        const result = await t.fetch("/", {
+          headers: {
+            Cookie: [
+              "Authorization=chunks.4; Path=/",
+              "Authorization.C1=00001; Path=/",
+              "Authorization.C2=00002; Path=/",
+              "Authorization.C3=00003; Path=/",
+              "Authorization.C4=00004; Path=/",
+            ].join("; "),
+          },
+        });
+        expect(result.headers.getSetCookie()).toEqual([
+          "Authorization.C3=; Max-Age=0; Path=/",
+          "Authorization.C4=; Max-Age=0; Path=/",
+          "Authorization=chunks.2; Path=/",
+          "Authorization.C1=00001; Path=/",
+          "Authorization.C2=00002; Path=/",
+        ]);
+        expect(await result.text()).toBe("200");
+      });
+    });
   });
 });
