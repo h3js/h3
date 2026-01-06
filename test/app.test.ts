8b9ca393ddf57bd76375d02dead68a4386915cd7
Pooya Parsa <pyapar@gmail.com>
2026-01-06 12:18:31 +0100

    fix(fromNodeHandler): pipe responses once (#1273)

--- test/app.test.ts
+++ test/app.test.ts
@@ -290,6 +290,44 @@ describeMatrix("app", (t, { it, expect }) => {
     },
   );
 
+  it.skipIf(t.target !== "node")("fromNodeHandler + piping", async () => {
+    t.app.all(
+      "/*",
+      fromNodeHandler((req, res) => {
+        const iterator = (async function* () {
+          yield "item1,";
+          yield "item2,";
+          yield "item3";
+        })();
+        NodeStreamReadable.from(iterator).pipe(res);
+      }),
+    );
+    const res = await t.fetch("/");
+    expect(res.status).toBe(200);
+    expect(await res.text()).toBe("item1,item2,item3");
+  });
+
+  it.skipIf(t.target !== "node")(
+    "fromNodeHandler + piping (with Error and custom status)",
+    async () => {
+      t.app.all(
+        "/*",
+        fromNodeHandler((req, res) => {
+          res.statusCode = 201;
+          const iterator = (async function* () {
+            yield "item1,";
+            yield "item2";
+            throw new Error("Test Error");
+          })();
+          NodeStreamReadable.from(iterator).pipe(res);
+        }),
+      );
+      const res = await t.fetch("/");
+      expect(res.status).toBe(201);
+      expect(await res.text()).toBe("item1,item2");
+    },
+  );
+
   it("set headers via event.res + Response (mutable)", async () => {
     t.app.use((event) => {
       event.res.headers.set("x-from-event", "1");
