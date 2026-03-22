43e1fa38ddcd13fa82558f754e4f5bd40e6aa4c8
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2026-03-22 10:36:20 +0100

    docs: remove `await-thenable` lint rule and fix invalid `await` usage (#1353)

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -18,7 +18,7 @@ export interface ProxyOptions {
   fetchOptions?: RequestInit & { duplex?: "half" | "full" };
   cookieDomainRewrite?: string | Record<string, string>;
   cookiePathRewrite?: string | Record<string, string>;
-  onResponse?: (event: H3Event, response: Response) => void;
+  onResponse?: (event: H3Event, response: Response) => void | Promise<void>;
 }
 
 /**
